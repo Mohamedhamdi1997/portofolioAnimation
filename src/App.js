cc68c2cfd99f98a279cc5ad75dcb26413292d2be
Mohamedhamdi1997 <medhamdi449@gmail.com>
2024-01-15 16:27:51 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import Aos from "aos";
+
+import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Hero from "./components/UI/Hero";
+import Service from "./components/UI/Service";
+
+function App() {
+  useEffect(() => {
+    Aos.init();
+  }, []);
+
+  return (
+    <>
+   <Header />
+   <main>
+    <Hero />
+    <Service />
+   </main>
+   <Footer />
+
+   
+    </>
+
+  );
+}
+
+export default App;
---
+++ src/assets/images/index.js
@@ -0,0 +1,7 @@
+import muhib from './muhib.png'
+import hero from './hero.svg'
+import hamma from './hamma.jpg'
+
+export {
+    muhib, hero, hamma
+}
